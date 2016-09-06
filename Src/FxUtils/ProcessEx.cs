a2ecf11dc9720663f592ba772a31fb3352d808d5
Lavnok <lavnok@users.noreply.github.com>
2016-09-06 17:28:45 +0530

    Initial commit of code
    At this checkpoint there are:
    1)  An organized source structure;
    2)  Three different projects;
    3)  A working PML to XML converter;
    4)  And a way to extract processlist information (and dump it to
    terminal).

---
+++ Src/FxUtils/ProcessEx.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace SeeBee.FxUtils
+{
+    public class ProcessEx
+    {
+        public ProcessEx(string fileName, string args, ProcessWindowStyle windowStyle = ProcessWindowStyle.Hidden)
+        {
+            Process = new Process();
+            Process.StartInfo.FileName = fileName;
+            Process.StartInfo.Arguments = args;
+            Process.StartInfo.WindowStyle = windowStyle;
+        }
+
+        public bool Start()
+        {
+            return Process.Start();
+        }
+
+        public int WaitForExitCode(int waitForMilliSec = 30000)
+        {
+            if (Process.WaitForExit(waitForMilliSec))
+            {
+                if (Process.HasExited)
+                {
+                    return Process.ExitCode;
+                }
+            }
+            return 0;
+        }
+
+        public Process Process
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ Src/PMLParser/PMLToXMLConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using SeeBee.FxUtils;
+
+namespace SeeBee.PMLParser
+{
+    public class PMLToXMLConverter
+    {
+        bool shouldRetryOnceOnFailure;
+        ProcessEx procMon;
+
+        #region Constructor
+        public PMLToXMLConverter(string procMonExeLocation, string pmlFile, bool shouldRetryOnceOnFailure = false)
+        {
+            this.shouldRetryOnceOnFailure = shouldRetryOnceOnFailure;
+            PMLFile = pmlFile;
+            // [BIB]:  http://stackoverflow.com/questions/5608980/how-to-ensure-a-timestamp-is-always-unique
+            XMLFile = string.Format("{0}_{1}.xml", FSUtils.GetFileName(pmlFile, true), DateTime.UtcNow.Ticks);
+            XMLFile = FSUtils.PathCombine(FSUtils.WritableLocationForTempFile, XMLFile);
+
+            StringBuilder sbArgs = new StringBuilder("/quiet /minimized /saveas2 ");
+            sbArgs.Append(XMLFile);
+            sbArgs.Append(" /openlog ");
+            sbArgs.Append(PMLFile);
+            // [BIB]:  http://forum.sysinternals.com/forum_posts.asp?TID=13843&PID=74632&title=producing-csv-from-process-monitor-via-script#74632
+            procMon = new ProcessEx(procMonExeLocation, sbArgs.ToString());
+        }
+        #endregion
+
+        public bool Convert()
+        {
+            procMon.Start();
+            if (0 == procMon.WaitForExitCode())
+            {
+                return true;
+            }
+            if (shouldRetryOnceOnFailure)
+            {
+                if (0 == procMon.WaitForExitCode())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #region Properties
+        public string PMLFile
+        {
+            get;
+            private set;
+        }
+
+        // [BIB]:  http://stackoverflow.com/a/2719761
+        public string XMLFile
+        {
+            get;
+            private set;
+        }
+        #endregion
+    }
+}
