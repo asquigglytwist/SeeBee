984d6c598408a3c3422c04e7d83a49028e68bd63
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-11 03:32:02 +0530

    Adding PMLAnalyzer as single point of invocation
    1)  Creating a new class - PMLAnalyzer;
    2)  Reducing scope of ProcessEntry and ProcessIntegrityLevel;
    3)  Moving part of the invocation code from Main to the newly added
    PMLAnalyzer.

--- Src/PMLParser/ProcessEntry.cs
+++ Src/PMLParser/ProcessEntry.cs
@@ -5,19 +5,23 @@ using System.Text;
 
 namespace SeeBee.PMLParser
 {
-    public class ProcessEntry
+    internal class ProcessEntry
     {
         public int ProcessId { get; set; }
+        public int ProcessIndex { get; set; }
         public int ParentProcessId { get; set; }
+        public int ParentProcessIndex { get; set; }
         public string AuthenticationId { get; set; }
         public long CreateTime { get; set; }
         public long FinishTime { get; set; }
         public bool IsVirtualized { get; set; }
         public bool Is64bit { get; set; }
-        public ProcessIntegrityLevel ProcessIntegriy { get; set; }
+        public ProcessIntegrityLevel ProcessIntegrity { get; set; }
+        public string ProcessName { get; set; }
+        public string CommandLine { get; set; }
     }
 
-    public enum ProcessIntegrityLevel
+    internal enum ProcessIntegrityLevel
     {
         Low,
         Medium,
