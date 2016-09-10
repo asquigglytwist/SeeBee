7dbd6fcd3cf9ae1011af97d78a0d3951224ddd2f
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-11 01:59:49 +0530

    Adding additional fields to ProcessEntry
    Adding a few more fields to ProcessEntry and an enum for
    ProcessIntegrityLevel.

--- Src/PMLParser/ProcessEntry.cs
+++ Src/PMLParser/ProcessEntry.cs
@@ -7,15 +7,20 @@ namespace SeeBee.PMLParser
 {
     public class ProcessEntry
     {
-        public int ProcessId
-        {
-            get;
-            set;
-        }
-        public int ParentProcessId
-        {
-            get;
-            set;
-        }
+        public int ProcessId { get; set; }
+        public int ParentProcessId { get; set; }
+        public string AuthenticationId { get; set; }
+        public long CreateTime { get; set; }
+        public long FinishTime { get; set; }
+        public bool IsVirtualized { get; set; }
+        public bool Is64bit { get; set; }
+        public ProcessIntegrityLevel ProcessIntegriy { get; set; }
+    }
+
+    public enum ProcessIntegrityLevel
+    {
+        Low,
+        Medium,
+        High
     }
 }
