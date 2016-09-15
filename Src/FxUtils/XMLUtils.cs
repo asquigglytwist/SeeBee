0df5a84d71088253a053a3ab2ae4c2ea3bc6b1b5
Lavnok <lavnok@users.noreply.github.com>
2016-09-16 02:25:24 +0530

    Cleaning up PMLModule creation
    1)  Cleaning up PMLModule creation with the addition of XmlElement
    version of APIs to XMLUtils;
    2)  Fixing few copy paste errors;
    3)  Cleaning up commented code.

--- Src/FxUtils/XMLUtils.cs
+++ Src/FxUtils/XMLUtils.cs
@@ -8,6 +8,7 @@ namespace SeeBee.FxUtils
 {
     public static class XMLUtils
     {
+        // XmlDocument version
         public static int ParseTagContentAsInt(XmlDocument xmlDoc, string tagName, int nodeIndex = 0, bool throwOnFailure = false)
         {
             string innerText = GetInnerText(xmlDoc, tagName, nodeIndex);
@@ -29,6 +30,35 @@ namespace SeeBee.FxUtils
         public static string GetInnerText(XmlDocument xmlDoc, string tagName, int nodeIndex = 0)
         {
             XmlNodeList nodeList = xmlDoc.GetElementsByTagName(tagName);
+            if (nodeList.Count > nodeIndex && nodeList[nodeIndex] != null)
+            {
+                return nodeList[nodeIndex].InnerText;
+            }
+            return null;
+        }
+
+        // XmlElement version
+        public static int ParseTagContentAsInt(XmlElement xmlElement, string tagName, int nodeIndex = 0, bool throwOnFailure = false)
+        {
+            string innerText = GetInnerText(xmlElement, tagName, nodeIndex);
+            return StringUtils.StringToInt(innerText, throwOnFailure);
+        }
+
+        public static long ParseTagContentAsLong(XmlElement xmlElement, string tagName, int nodeIndex = 0, bool throwOnFailure = false)
+        {
+            string innerText = GetInnerText(xmlElement, tagName, nodeIndex);
+            return StringUtils.StringToLong(innerText, throwOnFailure);
+        }
+
+        public static bool ParseTagContentAsBoolean(XmlElement xmlElement, string tagName, int nodeIndex = 0, bool throwOnFailure = false)
+        {
+            string innerText = GetInnerText(xmlElement, tagName, nodeIndex);
+            return StringUtils.StringToBoolean(innerText, throwOnFailure);
+        }
+
+        public static string GetInnerText(XmlElement xmlElement, string tagName, int nodeIndex = 0)
+        {
+            XmlNodeList nodeList = xmlElement.GetElementsByTagName(tagName);
             if (nodeList.Count > nodeIndex && nodeList[nodeIndex] != null)
             {
                 return nodeList[nodeIndex].InnerText;
