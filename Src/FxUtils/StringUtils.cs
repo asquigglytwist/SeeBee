85fdab9209150d6f5839738ca2dfb437d15c6c0e
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-16 02:09:39 +0530

    Expanding StringUtils and XMLUtils
    Expanding APIs for StringUtils and XMLUtils and cleaning up code in
    PMLProcess class - DRY.

--- Src/FxUtils/StringUtils.cs
+++ Src/FxUtils/StringUtils.cs
@@ -5,7 +5,49 @@ namespace SeeBee.FxUtils
 {
     public static class StringUtils
     {
-        public static long HexStringToLong(string hexString)
+        public static int StringToInt(string stringToConvert, bool throwOnFailure = false)
+        {
+            int convertedValue;
+            if (int.TryParse(stringToConvert, out convertedValue))
+            {
+                return convertedValue;
+            }
+            if (throwOnFailure)
+            {
+                throw new FormatException(string.Format("Unable to convert {0} to int.", stringToConvert));
+            }
+            return 0;
+        }
+
+        public static long StringToLong(string stringToConvert, bool throwOnFailure = false)
+        {
+            long convertedValue;
+            if (long.TryParse(stringToConvert, out convertedValue))
+            {
+                return convertedValue;
+            }
+            if (throwOnFailure)
+            {
+                throw new FormatException(string.Format("Unable to convert {0} to long.", stringToConvert));
+            }
+            return 0;
+        }
+
+        public static bool StringToBoolean(string stringToConvert, bool throwOnFailure = false)
+        {
+            bool convertedValue = false;
+            if (!stringToConvert.Equals("0"))
+            {
+                convertedValue = false;
+            }
+            if (throwOnFailure)
+            {
+                throw new FormatException(string.Format("Unable to convert {0} to bool.", stringToConvert));
+            }
+            return convertedValue;
+        }
+
+        public static long HexStringToLong(string hexString, bool throwOnFailure = false)
         {
             if (hexString.StartsWith("0x", StringComparison.CurrentCultureIgnoreCase))
             {
@@ -14,6 +56,10 @@ namespace SeeBee.FxUtils
             long value;
             if (!long.TryParse(hexString, System.Globalization.NumberStyles.HexNumber, CultureInfo.CurrentCulture, out value))
             {
+                if (throwOnFailure)
+                {
+                    throw new FormatException(string.Format("Unable to convert {0} to long.", hexString));
+                }
                 value = 0;
             }
             return value;
