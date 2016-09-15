2d3d6071d9e801757801388023ad0f0f36e108a3
Lavnok <lavnok@users.noreply.github.com>
2016-09-16 01:17:51 +0530

    Adding MathUtil
    Adding MathUtil to FxUtils with currently an API to parse hex string to
    long.

---
+++ Src/FxUtils/MathUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace SeeBee.FxUtils
+{
+    public static class MathUtils
+    {
+        public static long HexStringToLong(string hexString)
+        {
+            if (hexString.StartsWith("0x", StringComparison.CurrentCultureIgnoreCase))
+            {
+                hexString = hexString.Substring(2);
+            }
+            long value;
+            if (!long.TryParse(hexString, System.Globalization.NumberStyles.HexNumber, CultureInfo.CurrentCulture, out value))
+            {
+                value = 0;
+            }
+            return value;
+        }
+    }
+}
