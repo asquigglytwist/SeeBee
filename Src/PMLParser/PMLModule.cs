4f0ca5d1b749af7534217eba2d7c4a35f4976eb4
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-15 22:13:40 +0530

    Defining PMLModule
    Adding Properties and Constructor to PMLModule.

--- Src/PMLParser/PMLModule.cs
+++ Src/PMLParser/PMLModule.cs
@@ -5,7 +5,25 @@ using System.Text;
 
 namespace SeeBee.PMLParser
 {
-    class PMLModule
+    internal class PMLModule
     {
+        internal PMLModule(long timeStamp, long baseAddress, long size, string path, string version, string company, string description)
+        {
+            TimeStamp = timeStamp;
+            BaseAddress = baseAddress;
+            Size = size;
+            Path = path;
+            Version = version;
+            Company = company;
+            Description = description;
+        }
+
+        internal long TimeStamp { internal get; private set; }
+        internal long BaseAddress { internal get; private set; }
+        internal long Size { internal get; private set; }
+        internal string Path { internal get; private set; }
+        internal string Version { internal get; private set; }
+        internal string Company { internal get; private set; }
+        internal string Description { internal get; private set; }
     }
 }
