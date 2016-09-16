f03855c42d19ff5255be27d9a49e4e85b5f65a56
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-16 23:08:29 +0530

    Adding NumberUtils
    Adding NumberUtils - A class for grouping utility functions that take
    numerical input - generally including formatting functions but not
    limited to.

---
+++ Src/FxUtils/NumberUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SeeBee.FxUtils
+{
+    public static class NumberUtils
+    {
+
+        public static string LongToHexString(long longValue)
+        {
+            return longValue.ToString("X");
+        }
+
+        // [BIB]:  http://stackoverflow.com/questions/128618/c-file-size-format-provider
+        [DllImport("Shlwapi.dll", CharSet = CharSet.Auto)]
+        private static extern long StrFormatByteSize(long fileSize, [MarshalAs(UnmanagedType.LPTStr)] StringBuilder buffer, int bufferSize);
+
+        public static string FormatNumberAsFileSize(long fileSize)
+        {
+            StringBuilder buffer = new StringBuilder(20);
+            StrFormatByteSize(fileSize, buffer, buffer.Capacity);
+            return buffer.ToString();
+        }
+    }
+}
