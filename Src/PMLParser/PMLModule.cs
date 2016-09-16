240c920187b962a459d4907f178a109bf4ff0d6d
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-16 23:09:37 +0530

    Making PMLModule adaptive
    Making PMLModule adaptive by adding checks in object construction and
    overriding ToString for a more informative console dump.

--- Src/PMLParser/PMLModule.cs
+++ Src/PMLParser/PMLModule.cs
@@ -7,6 +7,8 @@ namespace SeeBee.PMLParser
 {
     public class PMLModule
     {
+        internal protected const string UnknownValue = "Unknown";
+
         internal static HashSet<int> LoadModules(XmlDocument processXMLDoc)
         {
             HashSet<int> processModuleList = new HashSet<int>();
@@ -18,6 +20,9 @@ namespace SeeBee.PMLParser
                 if (-1 == moduleIndex)
                 {
                     var tempModule = new PMLModule(path, module);
+#if DEBUG
+                    Console.WriteLine(tempModule);
+#endif
                     moduleIndex = PMLAnalyzer.AddModuleToList(tempModule);
                 }
                 if (-1 != moduleIndex)
@@ -36,10 +41,35 @@ namespace SeeBee.PMLParser
             TimeStamp = timeStamp;
             BaseAddress = baseAddress;
             Size = size;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("A module's path cannot be null or empty.");
+            }
             Path = path;
-            Version = version;
-            Company = company;
-            Description = description;
+            if (string.IsNullOrWhiteSpace(version))
+            {
+                Version = PMLModule.UnknownValue;
+            }
+            else
+            {
+                Version = version;
+            }
+            if (string.IsNullOrWhiteSpace(company))
+            {
+                Company = PMLModule.UnknownValue;
+            }
+            else
+            {
+                Company = company;
+            }
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                Description = PMLModule.UnknownValue;
+            }
+            else
+            {
+                Description = description;
+            }
         }
 
         internal PMLModule(string path, XmlElement module) :
@@ -75,6 +105,12 @@ namespace SeeBee.PMLParser
                 return true;
             }
             return false;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Module - {0} [version = {1}] of size {2} located at {3}, from {4}, was loaded at {5} into address 0x{6}.",
+                Description, Version, Size, Path, Company, TimeStamp, NumberUtils.LongToHexString(BaseAddress));
         }
     }
 }
