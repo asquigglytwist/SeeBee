ddf611eeb1156134cea149d3248567423cf852c3
Lavnok <lavnok@users.noreply.github.com>
2016-09-16 12:25:27 +0530

    Letting PMLModule handle its creation
    1)  Moving the construction of PMLModule logic to itself, from
    PMLProcess;
    2)  Changing TimeStamp to DateTime type instead of long.

--- Src/PMLParser/PMLModule.cs
+++ Src/PMLParser/PMLModule.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using SeeBee.FxUtils;
 
 namespace SeeBee.PMLParser
 {
     public class PMLModule
     {
-        internal PMLModule(long timeStamp, long baseAddress, long size, string path, string version, string company, string description)
+        internal static HashSet<int> LoadModules(XmlDocument processXMLDoc)
         {
-            TimeStamp = timeStamp;
+            HashSet<int> processModuleList = new HashSet<int>();
+            var modules = processXMLDoc.SelectNodes("/process/modulelist/module");
+            foreach (XmlElement module in modules)
+            {
+                string path = module.GetElementsByTagName("Path")[0].InnerText;
+                int moduleIndex = PMLAnalyzer.LocateModuleInList(path);
+                if (-1 == moduleIndex)
+                {
+                    var tempModule = new PMLModule(path, module);
+                    moduleIndex = PMLAnalyzer.AddModuleToList(tempModule);
+                }
+                if (-1 != moduleIndex)
+                {
+                    processModuleList.Add(moduleIndex);
+                }
+            }
+            return processModuleList;
+        }
+
+        private PMLModule(long timeStamp, long baseAddress, long size, string path, string version, string company, string description)
+        {
+#if DEBUG
+            Console.WriteLine("{0} was loaded at {1}.", path, DateTime.FromFileTime(timeStamp));
+#endif
+            TimeStamp = DateTime.FromFileTime(timeStamp);
             BaseAddress = baseAddress;
             Size = size;
             Path = path;
@@ -18,7 +44,19 @@ namespace SeeBee.PMLParser
             Description = description;
         }
 
-        internal long TimeStamp { get; private set; }
+        internal PMLModule(string path, XmlElement module) :
+            this(XMLUtils.ParseTagContentAsLong(module, "Timestamp"),
+            StringUtils.HexStringToLong(XMLUtils.GetInnerText(module, "BaseAddress")),
+            XMLUtils.ParseTagContentAsLong(module, "Size"),
+            path,
+            XMLUtils.GetInnerText(module, "Version"),
+            XMLUtils.GetInnerText(module, "Company"),
+            XMLUtils.GetInnerText(module, "Description"))
+        {
+        }
+
+        internal int ModuleIndex { get; private set; }
+        internal DateTime TimeStamp { get; private set; }
         internal long BaseAddress { get; private set; }
         internal long Size { get; private set; }
         internal string Path { get; private set; }
