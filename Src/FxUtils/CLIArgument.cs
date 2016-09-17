07d6f775f9bf6ef66698de97f5835b16b46bdbe7
Lavnok <lavnok@users.noreply.github.com>
2016-09-18 01:36:57 +0530

    Adding CLIArgument class
    Adding CLIArgument class to deal with individual arguments.

---
+++ Src/FxUtils/CLIArgument.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeeBee.FxUtils
+{
+    public class CLIArgument
+    {
+        #region Constructor
+        public CLIArgument(string shortVersion, string name, bool isRequired, string[] parameterNames, string explanation,
+            bool isCaseSensitive = false, string sampleUsage = null, CLIArgument nestedArgument = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Argument's name cannot be null or empty.");
+            }
+            ShortVersion = shortVersion;
+            Name = name;
+            IsRequired = isRequired;
+            ParameterNames = parameterNames;
+            Explanation = explanation;
+
+            IsCaseSensitive = isCaseSensitive;
+            SampleUsage = sampleUsage;
+            NestedArgument = nestedArgument;
+        }
+        #endregion
+
+        #region Properties
+        public string ShortVersion { get; private set; }
+        public string Name { get; private set; }
+        public bool IsRequired { get; private set; }
+        public string[] ParameterNames { get; private set; }
+        public string Explanation { get; private set; }
+
+        public bool IsCaseSensitive { get; private set; }
+        public string SampleUsage { get; private set; }
+        public CLIArgument NestedArgument { get; private set; }
+        #endregion
+    }
+}
