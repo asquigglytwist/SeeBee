48b1038bb376aad08ecc9152159ba0f471dd811d
Lavnok <lavnok@users.noreply.github.com>
2016-09-18 00:49:58 +0530

    Simplifying CLIArgsParser creation
    Simplifying CLIArgsParser creation - Now with only one parameter which
    is a default;
    Correcting minor issue where args was received as a single string
    instead of as an array;
    Adding test code in Main for testing the CLIArgsParser.

--- Src/SeeBeeCmd/Program.cs
+++ Src/SeeBeeCmd/Program.cs
@@ -12,6 +12,19 @@ namespace SeeBee.SeeBeeCmd
             pmlAnalyzer = new PMLAnalyzer();
             pmlAnalyzer.Init(args);
 #else
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write("Arg at {0};", i);
+                int j = args[i].IndexOf(':');
+                if (j > -1)
+                {
+                    Console.WriteLine("\nOption:   {0};\nArgument:   {1}", args[i].Substring(0, j), args[i].Substring(j + 1));
+                }
+                else
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
             pmlAnalyzer = new PMLAnalyzer(@"C:\T\SeeBee\Procmon.exe");
             pmlAnalyzer.ProcessPMLFile(@"C:\T\SeeBee\Logfile.PML");
             Console.ReadKey(true);
