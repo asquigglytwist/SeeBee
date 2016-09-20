5ce4c8b5fa66f7dc878d386c478ad0f079b6fffd
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-20 20:03:37 +0530

    Adding temporary CLIArgs
    Adding temporary CLIArgs for both testing as well as a starting point.

--- Src/PMLParser/PMLAnalyzer.cs
+++ Src/PMLParser/PMLAnalyzer.cs
@@ -15,6 +15,28 @@ namespace SeeBee.PMLParser
         {
             globalModuleList.Add(PMLModule.System);
             globalOwnerList.Add("NT AUTHORITY\\SYSTEM");
+            InitAllCLIArgs();
+        }
+
+        private static void InitAllCLIArgs()
+        {
+            CLIArgument[] cliArgs = new CLIArgument[]
+            {
+                new CLIArgument("pm", "procmon", true, new string[] { "filename" }, "Path to the ProcMon (Process Monitor) executable.", @"in C:\SysInternals\ProcMon\ProcMon.exe"),
+                new CLIArgument("in", "inputfile", true, new string[] { "filename" }, "The input Process Monitor Log (PML) file that is to be processed.", @"in C:\Logs\LogFile.PML"),
+                new CLIArgument("out", "outputfile", false, new string[] { "filename" }, "Location for the output XML file to be stored.", @"out C:\Logs\AfterConversion.XML"),
+                new CLIArgument("pid", "processid", true, new string[] { "pid" }, "PID of the process to be filtered.", "pid 1234"),
+                new CLIArgument("im", "image", false, new string[] { "imagename" }, "Image name of the process to be filtered.", "im explorer.exe"),
+                new CLIArgument("ip", "ignorepass", false, null, "Ignores all opertaions for which the result is SUCCESS.", "ip"),
+                new CLIArgument(null, "result", false, new string[] { "result" }, "Filters results based on the value specified.  Accepts \"pass\" and \"fail\" as the only valid values.", "result pass"),
+                new CLIArgument("fi", "filter", false, new string[] { "field", "operator", "value" }, "Filters results based on the value specified.  Accepts \"pass\" and \"fail\" as the only valid values.", "result pass")
+            };
+#if DEBUG
+            foreach (var arg in cliArgs)
+            {
+                Console.WriteLine(arg.ToString());
+            }
+#endif
         }
 
         internal static int LocateOwnerInList(string owner)
