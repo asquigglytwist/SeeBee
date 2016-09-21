8b1237fbbf788a4739fe36518435ddd4bacfeb3d
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-21 21:04:49 +0530

    Attempting to parse CLI Args
    Attempting to parse CLI Args with CLIArgsParser's Parse method.

--- Src/FxUtils/CLIArgsParser.cs
+++ Src/FxUtils/CLIArgsParser.cs
@@ -27,6 +27,18 @@ namespace SeeBee.FxUtils
         #region Methods
         public void Parse(string[] args, ref CLIArgument[] cliArgs)
         {
+            for (int i = 0; i < args.Length; i++)
+            {
+                CLIArgument cliArg = (CLIArgument)from arg in cliArgs
+                                                  where (arg.Name.Equals(args[i]) || arg.ShortVersion.Equals(args[i]))
+                                                  select arg;
+                if (cliArg.ParameterNames != null)
+                {
+                    for (int j = 0; j < cliArg.ParameterNames.Length; j++)
+                    {
+                    }
+                }
+            }
         }
         #endregion
     }
