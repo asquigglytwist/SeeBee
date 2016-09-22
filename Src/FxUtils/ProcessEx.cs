93523c1cbdcd34051e369a2bb66ae047a5662fc3
Lavnok <lavnok@users.noreply.github.com>
2016-09-23 00:19:08 +0530

    Making PMLAnalyzer CLI Args aware
    1)  PMLAnalyzer parses CLI Args for configuration information instead of
    being provided directly from Main;
    2)  CLIArgsParser is capable of Parsing arguments and return them as a
    configuration dictionary;
    3)  Adding a try-catch to ProcessEx to handle invalid process Start()
    requests.

--- Src/FxUtils/ProcessEx.cs
+++ Src/FxUtils/ProcessEx.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace SeeBee.FxUtils
 {
@@ -14,7 +15,14 @@ namespace SeeBee.FxUtils
 
         public bool Start()
         {
-            return Process.Start();
+            try
+            {
+                return Process.Start();
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
         }
 
         public int WaitForExitCode(int waitForMilliSec = 30000)
