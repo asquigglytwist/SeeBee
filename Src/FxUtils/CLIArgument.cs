033e7e29eeab945702bbfb141b60d4b94449a069
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-24 20:51:13 +0530

    Adding ToLongString in CLIArgument
    1)  Adding ToLongString in CLIArgument;
    2)  Making an additional check if NestedArgument will lead into infinite
    recursion.

--- Src/FxUtils/CLIArgument.cs
+++ Src/FxUtils/CLIArgument.cs
@@ -24,6 +24,10 @@ namespace SeeBee.FxUtils
             SampleUsage = sampleUsage;
             IsCaseSensitive = isCaseSensitive;
             NestedArgument = nestedArgument;
+            if (this.Equals(NestedArgument))
+            {
+                throw new ArgumentException(string.Format("Argument cannot match NestedArgument for command {0}.", Name));
+            }
         }
         #endregion
 
@@ -77,6 +81,37 @@ namespace SeeBee.FxUtils
         public string ToShortString()
         {
             return string.Format("{0}{1}{2}", (this.IsRequired ? "" : "["), this.Name, (this.IsRequired ? "" : "]"));
+        }
+
+        public string ToLongString()
+        {
+            StringBuilder buffer = new StringBuilder(string.Format("{0}{1}{2}",
+                (this.IsRequired ? "" : "["), CLIArgsParser.DefaultOptionDelimiter, this.Name));
+            if (!string.IsNullOrWhiteSpace(this.ShortVersion))
+            {
+                buffer.AppendFormat(" [or {0}{1}]", CLIArgsParser.DefaultOptionDelimiter, this.ShortVersion);
+            }
+            if (this.ParameterNames != null)
+            {
+                for (int i = 0; i < this.ParameterNames.Length; i++)
+                {
+                    buffer.AppendFormat(" {0}", this.ParameterNames[i]);
+                }
+            }
+            if (this.NestedArgument != null)
+            {
+                buffer.AppendFormat(" {0}{1}", this.NestedArgument.ToString());
+            }
+            buffer.Append((this.IsRequired ? "" : "]"));
+            if (Explanation != null)
+            {
+                buffer.AppendFormat("{0}A brief explanation:{0}{1}{0}\t", Environment.NewLine, Explanation);
+            }
+            if (SampleUsage != null)
+            {
+                buffer.AppendFormat("{0}An example of the command's usage:{0}{1}{0}\t", Environment.NewLine, SampleUsage);
+            }
+            return buffer.ToString();
         }
     }
 }
