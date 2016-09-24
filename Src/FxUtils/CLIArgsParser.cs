6e11c2e6385140fb6c1403b1052ebb37c1a26026
Lavnok <lavnok@users.noreply.github.com>
2016-09-24 20:54:10 +0530

    Making CLIArgsParser responsive to errors
    Making CLIArgsParser responsive to errors by returning an error message
    back to caller which will act as a guide as well as information.

--- Src/FxUtils/CLIArgsParser.cs
+++ Src/FxUtils/CLIArgsParser.cs
@@ -25,8 +25,9 @@ namespace SeeBee.FxUtils
         #endregion
 
         #region Methods
-        public void Parse(string[] args, CLIArgument[] cliArgs, Dictionary<string, List<string>> parsedArguments)
+        public string Parse(string[] args, CLIArgument[] cliArgs, Dictionary<string, List<string>> parsedArguments)
         {
+            string errorMsg = string.Empty;
             for (int i = 0; i < args.Length; i++)
             {
                 List<string> paramsList = null;
@@ -34,17 +35,38 @@ namespace SeeBee.FxUtils
                               where (arg.Name.Equals(args[i]) ||
                               (arg.ShortVersion != null && arg.ShortVersion.Equals(args[i])))
                               select arg).FirstOrDefault();
+                if (cliArg == null)
+                {
+                    if (!string.IsNullOrWhiteSpace(errorMsg))
+                    {
+                        errorMsg += Environment.NewLine;
+                    }
+                    errorMsg += string.Format("Unable to parse / recognize Command Line Argument \"{0}\" at position {1}.", args[i], i);
+                    continue;
+                }
                 if (cliArg.ParameterNames != null)
                 {
                     paramsList = new List<string>(cliArg.ParameterNames.Length);
                     for (int j = 0; j < cliArg.ParameterNames.Length; j++)
                     {
-                        paramsList.Add(args[i + j + 1]);
+                        try
+                        {
+                            paramsList.Add(args[i + j + 1]);
+                        }
+                        catch(IndexOutOfRangeException)
+                        {
+                            if (!string.IsNullOrWhiteSpace(errorMsg))
+                            {
+                                errorMsg += Environment.NewLine;
+                            }
+                            errorMsg += string.Format("Unable to parse Command Line Argument \"{0}\" at position {1}; Not enough arguments.  {2}For more details on the usage of the parameter,{2}{3}", args[i], i, Environment.NewLine, cliArg.ToLongString());
+                        }
                     }
                     i += cliArg.ParameterNames.Length;
                 }
                 parsedArguments[cliArg.Name] = paramsList;
             }
+            return errorMsg;
         }
         #endregion
     }
