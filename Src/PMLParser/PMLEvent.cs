9478fe76d0e9794a9b6dd0598dece246ab00ccbb
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-09-26 00:03:03 +0530

    Prefixing members with this keyword
    Prefixing all members - Properties and Variables with the "this"
    keyword.

--- Src/PMLParser/PMLEvent.cs
+++ Src/PMLParser/PMLEvent.cs
@@ -9,7 +9,7 @@ namespace SeeBee.PMLParser
         #region Properties
         internal int ProcessIndex { get; private set;}
         internal DateTime TimeOfDay { get; private set;}
-        internal string Process_Name { get; private set;}
+        internal string ProcessName { get; private set;}
         internal int PID { get; private set;}
         internal int TID { get; private set;}
         internal ProcessIntegrityLevel Integrity { get; private set;}
@@ -26,18 +26,18 @@ namespace SeeBee.PMLParser
         {
             XmlDocument eventXMLDoc = new XmlDocument();
             eventXMLDoc.Load(eventListReader);
-            ProcessIndex = XMLUtils.ParseTagContentAsInt(eventXMLDoc, TagNames.Event_ProcessIndex);
-            TimeOfDay = XMLUtils.ParseTagContentAsFileTime(eventXMLDoc, TagNames.Event_TimeOfDay);
-            Process_Name = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Process_Name);
-            PID = XMLUtils.ParseTagContentAsInt(eventXMLDoc, TagNames.Event_PID);
-            TID = XMLUtils.ParseTagContentAsInt(eventXMLDoc, TagNames.Event_TID);
-            Integrity = ProcessIntegrityLevelStrings.ParseString(XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Integrity));
-            Sequence = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Sequence);
-            Virtualized = XMLUtils.ParseTagContentAsBoolean(eventXMLDoc, TagNames.Event_Virtualized);
-            Operation = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Operation);
-            Path = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Path);
-            Result = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Result);
-            Detail = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Detail);
+            this.ProcessIndex = XMLUtils.ParseTagContentAsInt(eventXMLDoc, TagNames.Event_ProcessIndex);
+            this.TimeOfDay = XMLUtils.ParseTagContentAsFileTime(eventXMLDoc, TagNames.Event_TimeOfDay);
+            this.ProcessName = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Process_Name);
+            this.PID = XMLUtils.ParseTagContentAsInt(eventXMLDoc, TagNames.Event_PID);
+            this.TID = XMLUtils.ParseTagContentAsInt(eventXMLDoc, TagNames.Event_TID);
+            this.Integrity = ProcessIntegrityLevelStrings.ParseString(XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Integrity));
+            this.Sequence = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Sequence);
+            this.Virtualized = XMLUtils.ParseTagContentAsBoolean(eventXMLDoc, TagNames.Event_Virtualized);
+            this.Operation = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Operation);
+            this.Path = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Path);
+            this.Result = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Result);
+            this.Detail = XMLUtils.GetInnerText(eventXMLDoc, TagNames.Event_Detail);
         }
         #endregion
 
@@ -65,7 +65,7 @@ namespace SeeBee.PMLParser
         {
             return string.Format("Thread {0} of Process {1} [PID: {2}] performed {3} at {4} on {5} and result was {6}.{7}Details:{8}{7}",
                 this.TID,
-                this.Process_Name,
+                this.ProcessName,
                 this.PID,
                 this.Operation,
                 this.TimeOfDay,
