192cc07fd2381795051023581fa7223b4d2e364f
Lavnok <lavnok@users.noreply.github.com>
2016-09-27 01:41:45 +0530

    Adding IndexedStringCollection
    Introducing a new class - IndexedStringCollection for storing string to
    integer mapping; Will currently aid in the lists used for PMLModule
    paths and Process Owner names.

---
+++ Src/FxUtils/IndexedStringCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeeBee.FxUtils
+{
+    public class IndexedStringCollection : IEnumerable<KeyValuePair<string, int>>
+    {
+        #region Properties
+        public int LastAddedIndex { get; protected internal set; }
+        protected internal Dictionary<string, int> IndexedStringDictionary { get; protected internal set; }
+        #endregion
+
+        #region Constructor
+        public IndexedStringCollection()
+        {
+            LastAddedIndex = -1;
+            IndexedStringDictionary = new Dictionary<string, int>();
+        }
+        #endregion
+
+        #region Public Methods
+        public int LocateString(string stringToFind)
+        {
+            int indexOfString;
+            if (!IndexedStringDictionary.TryGetValue(stringToFind, out indexOfString))
+            {
+                indexOfString = -1;
+            }
+            return indexOfString;
+        }
+
+        public int Add(string stringToAdd, bool throwOnFail = false)
+        {
+            int indexOfString = LocateString(stringToAdd);
+            if (indexOfString > -1)
+            {
+                if (throwOnFail)
+                {
+                    throw new ArgumentException(string.Format("String {0} already exists.", stringToAdd));
+                }
+                return indexOfString;
+            }
+            IndexedStringDictionary[stringToAdd] = ++LastAddedIndex;
+            return LastAddedIndex;
+        }
+        #endregion
+
+        #region IEnumberable
+        IEnumerator<KeyValuePair<string, int>> IEnumerable<KeyValuePair<string, int>>.GetEnumerator()
+        {
+            return IndexedStringDictionary.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return IndexedStringDictionary.GetEnumerator();
+        }
+        #endregion
+    }
+}
