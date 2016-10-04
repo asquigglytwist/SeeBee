b50de2b8bf7b6d90672ac7803b014c989ea20bc6
Lavnok <lavnok@users.noreply.github.com>
2016-10-05 01:28:36 +0530

    Handling construction of PMLStackFrame
    Handling construction of PMLStackFrame;
    Adding a static method LoadStackFrames to deserialize the input XML
    content;
    Updating corresponding TagNames.

--- Src/PMLParser/PMLStackFrame.cs
+++ Src/PMLParser/PMLStackFrame.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SeeBee.FxUtils;
 
 namespace SeeBee.PMLParser
 {
     internal class PMLStackFrame
     {
         int pathIndex;
+        internal const string UnknownStringValue = "<unknown>";
+
+        #region Static Methods
+        internal static PMLStackFrame[] LoadStackFrames(XmlDocument eventXMLDoc)
+        {
+            var stackFrames = eventXMLDoc.SelectNodes(TagNames.StackFrame_XPathInXML);
+            PMLStackFrame[] eventStackFrames = new PMLStackFrame[stackFrames.Count];
+            int i = 0;
+            foreach (XmlElement frame in stackFrames)
+            {
+                eventStackFrames[i++] = new PMLStackFrame(frame);
+            }
+            return eventStackFrames;
+        }
+        #endregion
+
+        #region Constructor
+        private PMLStackFrame(long address, string path, string location)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                //throw new ArgumentException("A StackFrame cannot have null or empty path.");
+                path = UnknownStringValue;
+            }
+            if (string.IsNullOrWhiteSpace(location))
+            {
+                //throw new ArgumentException("A StackFrame cannot have null or empty location.");
+                location = UnknownStringValue;
+            }
+            this.Address = address;
+            this.pathIndex = PMLAnalyzer.AddFilePathToList(path);
+            this.Location = location;
+        }
+
+        internal PMLStackFrame(XmlElement frame) :
+            this(StringUtils.HexStringToLong(XMLUtils.GetInnerText(frame, TagNames.StackFrame_Address)),
+            XMLUtils.GetInnerText(frame, TagNames.StackFrame_Path),
+            XMLUtils.GetInnerText(frame, TagNames.StackFrame_Location))
+        {
+        }
+        #endregion
 
         #region Properties
         internal long Address { get; private set; }
