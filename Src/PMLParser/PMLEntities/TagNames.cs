e27b9c2eb28b99a5168370477825f7844423c3ea
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-10-08 19:39:39 +0530

    Moving TagNames to PMLEntities
    Moving TagNames to PMLEntities solution folder and updating the
    namespace accordingly.

--- Src/PMLParser/PMLEntities/TagNames.cs
+++ Src/PMLParser/PMLEntities/TagNames.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SeeBee.PMLParser
+namespace SeeBee.PMLParser.PMLEntities
 {
     internal static class TagNames
     {
