bbe5d9a5333684c0fdf230bde05288827e3645cc
Lavnok <lavnok@users.noreply.github.com>
2016-10-08 19:57:41 +0530

    Moving CLIArg* under CLIArgs
    Moving CLIArg* APIs under CLIArgs Solution folder and updating
    namespaces accordingly along with references to it.

--- Src/FxUtils/CLIArgs/CLIArgsParser.cs
+++ Src/FxUtils/CLIArgs/CLIArgsParser.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace SeeBee.FxUtils
+namespace SeeBee.FxUtils.CLIArgs
 {
     public class CLIArgsParser
     {
--- Src/PMLParser/PMLAnalyzer.cs
+++ Src/PMLParser/PMLAnalyzer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using SeeBee.FxUtils;
+using SeeBee.FxUtils.CLIArgs;
 using SeeBee.FxUtils.Utils;
 using SeeBee.PMLParser.Conversion;
 using SeeBee.PMLParser.PMLEntities;
