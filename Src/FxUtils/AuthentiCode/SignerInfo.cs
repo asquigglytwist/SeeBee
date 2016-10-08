ad4ddccf65065c956f9c4b7e93dae9c5c08ac203
Lavnok <lavnok@users.noreply.github.com>
2016-10-08 17:47:34 +0530

    Adding a new SignerInfo class
    Adding a new SignerInfo class that extracts the digital signature
    information from the PE binary and verify if it meets expectations;
    Using above class to verify that provided ProcMon executable is a
    trusted binary and that it is signed by Microsoft.

---
+++ Src/FxUtils/AuthentiCode/SignerInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SeeBee.FxUtils.AuthentiCode
+{
+    public static class SignerInfo
+    {
+        // [BIB]:  http://stackoverflow.com/questions/28556981/c-sharp-how-to-get-dll-or-exe-files-digital-signer-certificate-info-even-the-c
+        public static X509Certificate GetSignerInfo(string inputBinary)
+        {
+            return X509Certificate.CreateFromSignedFile(inputBinary);
+        }
+
+        public static bool IsSignedBy(string inputBinary, string expectedSignerName, bool exactMatch = false)
+        {
+            var cert = GetSignerInfo(inputBinary);
+            bool meetsSigningRequirements = (exactMatch ? cert.Issuer.Equals(expectedSignerName) : cert.Issuer.Contains(expectedSignerName));
+            return meetsSigningRequirements;
+        }
+    }
+}
