49d4b029f23b4cbe249a740d236658f7554b55b2
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-10-10 00:07:29 +0530

    Adding PMLFile class
    Adding PMLFile class to handle the combined information on other PML
    entities.

---
+++ Src/PMLParser/PMLEntities/PMLFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeeBee.PMLParser.PMLEntities
+{
+    /// <summary>
+    /// Represents an input PML file
+    /// </summary>
+    class PMLFile
+    {
+        internal PMLProcess[] processes;
+        internal PMLEvent[] events;
+
+        internal PMLFile(PMLProcess[] procs, PMLEvent[] evts)
+        {
+            this.processes = procs;
+            this.events = evts;
+        }
+    }
+}
