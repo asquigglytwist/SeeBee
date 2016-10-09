7c80eb2ab06877e5e9669c7ad94444384064c936
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2016-10-10 00:26:12 +0530

    Adding AppConfig and AppConfigPropertyNames
    Adding AppConfig and AppConfigPropertyNames under ConfigManager
    namespace and solution folder.

---
+++ Src/PMLParser/ConfigManager/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using SeeBee.FxUtils.Utils;
+using SeeBee.PMLParser.ManagedLists;
+
+namespace SeeBee.PMLParser.ConfigManager
+{
+    internal class AppConfig
+    {
+        List<int> ignoredProcessNames;
+        internal AppConfig(string configFile)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(configFile);
+            string[] csvProcessNames = XMLUtils.GetInnerText(doc, AppConfigPropertyNames.IgnoredProcessNames).Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries);
+            ignoredProcessNames = new List<int>(csvProcessNames.Length);
+            foreach (string s in csvProcessNames)
+            {
+                ignoredProcessNames.Add(ProcessNameList.AddProcessNameToList(s));
+            }
+        }
+    }
+}
