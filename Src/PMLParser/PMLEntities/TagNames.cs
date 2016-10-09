3128ecdcda7c879d8935c781f838d3c995ca70cb
Lavnok <lavnok@users.noreply.github.com>
2016-10-09 17:20:50 +0530

    Cleaning up code in TagNames
    Cleaning up code in PMLEntities' TagNames.

--- Src/PMLParser/PMLEntities/TagNames.cs
+++ Src/PMLParser/PMLEntities/TagNames.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SeeBee.PMLParser.PMLEntities
+﻿namespace SeeBee.PMLParser.PMLEntities
 {
     internal static class TagNames
     {
